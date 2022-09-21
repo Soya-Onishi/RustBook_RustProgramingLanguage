0e06abe60e0834997606c83a6f3344807dd97e75
Soya Ohnishi <soya.ohnishi@gmail.com>
2022-09-21 18:30:00 +0900

    8章の文字列に関するサンプルコードを追加

--- common_collections/src/main.rs
+++ common_collections/src/main.rs
@@ -52,4 +52,64 @@ fn main() {
             SpreadsheetCell::Float(10.12),
         ];
     }
+
+    {
+        let data = "initial contents";
+        let s = data.to_string();
+        println!("{}", s);
+        let s = "initial contents".to_string();
+        println!("{}", s);
+    }
+
+    // 文字列の更新
+    {
+        let mut s = String::from("foo");
+        s.push_str("bar");
+        println!("{}", s);
+        
+        let mut s1 = String::from("foo");
+        let s2 = "bar";
+        s1.push_str(s2);
+        println!("{},{}", s1, s2);        
+
+        let mut s = String::from("lo");
+        s.push('l');
+        println!("{}", s);
+    }
+
+    // formatによる文字列の結合
+    {
+        let s1 = String::from("tic");
+        let s2 = String::from("tac");
+        let s3 = String::from("toe");
+
+        let s = format!("{}-{}-{}", s1, s2, s3);
+
+        println!("{}", s);
+    }
+
+    // 文字列のスライス
+    {
+        let hello = "Здравствуйте";
+        let s1 = &hello[0..4];
+
+        // 各文字のバイト長と合わないためpanic
+        // let s2 = &hello[0..1];
+
+        println!("{}", s1);
+    }
+
+    // 文字列の走査
+    {
+        println!("print chars");
+        let msg = "नमस्ते";
+        for c in msg.chars() {
+            println!("{}", c);
+        }
+
+        println!("print bytes");
+        for b in msg.bytes() {
+            println!("{}", b);
+        }
+    }
 }
