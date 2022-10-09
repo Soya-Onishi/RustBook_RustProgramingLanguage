fa4d9c90efe5190817582f1953213d3bdd37fa5b
Soya Ohnishi <soya.ohnishi@gmail.com>
2022-10-09 23:58:54 +0900

    エラーハンドリングのコード追加

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -1,3 +1,25 @@
+use std::fs::File;
+
 fn main() {
-    println!("Hello, world!");
+    // simple_panic();
+    // panic_by_out_of_bounds();
+    open_file();
+}
+
+fn simple_panic() {
+    panic!("crash and burn");
+}
+
+fn panic_by_out_of_bounds() {
+    let v = vec![1, 2, 3];
+
+    v[99];
+}
+
+fn open_file() {
+    let f = File::open("./hello.txt");
+    let _f = match f {
+        Ok(file) => file,
+        Err(err) => panic!("There was a problem opening the file: {:?}", err)
+    };
 }
