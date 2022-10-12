8dd032895baf6435f135eddf83183078803917b5
Soya Ohnishi <soya.ohnishi@gmail.com>
2022-10-12 14:44:58 +0900

    テスト例をいくつか追加

--- unit_test/adder/src/lib.rs
+++ unit_test/adder/src/lib.rs
@@ -10,6 +10,21 @@ impl Rectangle {
     }
 }
 
+pub struct Guess {
+    #[allow(dead_code)]
+    value: i32
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}", value);
+        }
+
+        Guess { value }
+    }
+}
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
@@ -36,5 +51,34 @@ mod tests {
         };
 
         assert!(larger.can_hold(&smaller));
+    }
+
+    #[test]
+    fn smaller_cannot_hold_larger() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(!smaller.can_hold(&larger));
+    }
+
+    #[test]
+    #[should_panic(expected = "Guess value must be between 1 and 100, got 200")]
+    fn greater_than_100() {
+        Guess::new(200);
+    }
+
+    #[test]
+    fn with_result() -> Result<(), String> {
+        if 2 + 2 == 4 {
+            Ok(())
+        } else {
+            Err(String::from("two plus two does not equal four"))
+        }
     }
 }
