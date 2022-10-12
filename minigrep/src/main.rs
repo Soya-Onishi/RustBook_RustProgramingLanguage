3d33becc8882a2bcc5d06e4c6f12d40992a0904e
Soya Ohnishi <soya.ohnishi@gmail.com>
2022-10-12 17:46:02 +0900

    main関数から処理を分離

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,16 +1,47 @@
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+use std::process;
+use std::error::Error;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    
-    let query = &args[1];
-    let filename = &args[2];
+    let args: Vec<String> = env::args().collect();    
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    let mut f = File::open(filename).expect("file not found");
+    if let Err(err) = run(config) {
+        println!("Application error: {}", err);
+        process::exit(1);
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let mut f = File::open(config.filename)?;
     let mut contents = String::new();
-    f.read_to_string(&mut contents).expect("something went wrong reading the file");
+    f.read_to_string(&mut contents)?;
 
     println!("text: {}", contents);
+
+    Ok(())
 }
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments")
+        }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+    
+        Ok(Config { query, filename })
+    }
+}
+
