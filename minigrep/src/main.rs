2a350e4f15e7bf484a7952d09cfc636d06160b82
Soya Ohnishi <soya.ohnishi@gmail.com>
2022-10-12 18:25:26 +0900

    クエリ文字列の検索部分の処理を追加

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,8 +1,8 @@
+extern crate minigrep;
+
 use std::env;
-use std::fs::File;
-use std::io::prelude::*;
 use std::process;
-use std::error::Error;
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();    
@@ -11,37 +11,8 @@ fn main() {
         process::exit(1);
     });
 
-    if let Err(err) = run(config) {
+    if let Err(err) = minigrep::run(config) {
         println!("Application error: {}", err);
         process::exit(1);
     }
 }
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let mut f = File::open(config.filename)?;
-    let mut contents = String::new();
-    f.read_to_string(&mut contents)?;
-
-    println!("text: {}", contents);
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments")
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-    
-        Ok(Config { query, filename })
-    }
-}
-
