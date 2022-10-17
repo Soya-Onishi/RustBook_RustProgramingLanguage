67528e76765b4e771bf03bbe9d010c0e331ccdbe
SoyaOhnishi <soya.ohnishi@gmail.com>
2022-10-17 23:14:27 +0900

    チャンネルによるスレッド間のデータの受け渡しのサンプルを記述

--- multi_threads/src/main.rs
+++ multi_threads/src/main.rs
@@ -1,5 +1,6 @@
 use std::thread;
 use std::time::Duration;
+use std::sync::mpsc;
 
 fn main() {
     let v = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
@@ -18,4 +19,50 @@ fn main() {
     }
 
     handle.join().unwrap();
+
+    let (tx, rx) = mpsc::channel();
+    thread::spawn(move || {
+        let val = String::from("hi");
+        tx.send(val).unwrap();
+    });
+
+    let received = rx.recv().unwrap();
+    println!("Got: {}", received);
+
+    let (tx, rx) = mpsc::channel();
+    thread::spawn(move || {
+        let vals = vec![1, 2, 3, 4];
+
+        for v in vals {
+            tx.send(v).unwrap();
+            thread::sleep(Duration::from_millis(1000));
+        }
+    });
+
+    for received in rx {
+        println!("Got Numbers: {}", received);
+    }
+
+    let (tx, rx) = mpsc::channel();
+    let tx1 = mpsc::Sender::clone(&tx);
+
+    thread::spawn(move || {
+        let vals = vec![10, 20, 30, 40];
+        for v in vals {
+            tx.send(v).unwrap();
+            thread::sleep(Duration::from_millis(1000));
+        }        
+    });
+
+    thread::spawn(move || {
+        let vals = vec![5, 15, 25, 35];
+        for v in vals {
+            tx1.send(v).unwrap();
+            thread::sleep(Duration::from_millis(1000));
+        }
+    });
+
+    for received in rx {
+        println!("Got Numbers from multiple Tx: {}", received);
+    }
 }
