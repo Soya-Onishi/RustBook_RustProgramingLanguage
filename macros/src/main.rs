78f0cbf22f726f87c6131a6180631692e47aa1f7
SoyaOhnishi <soya.ohnishi@gmail.com>
2022-10-30 07:56:14 +0900

    マクロに関するチュートリアルを実施

---
+++ macros/src/main.rs
@@ -0,0 +1,26 @@
+use hello_macro::HelloMacro;
+use hello_macro_derive::HelloMacro;
+
+#[derive(HelloMacro)]
+struct MyStruct;
+
+#[macro_export]
+macro_rules! my_vec {
+    ( $( $x:expr ),* ) => {
+        {
+            let mut temp_vec = Vec::new();
+            $(
+                temp_vec.push($x);
+            )*
+            temp_vec
+        }
+    };
+}
+
+fn main() {
+    let v = my_vec!(1, 2, 3);
+
+    println!("{:?}", v);
+
+    MyStruct::hello_macro();
+}
