b97d4b1883144c41494530b5def95b657c1b4d38
SoyaOhnishi <soya.ohnishi@gmail.com>
2022-10-30 12:01:06 +0900

    スレッドプールによる複数の要求への対応の並列化を実装

--- http_hello/src/bin/main.rs
+++ http_hello/src/bin/main.rs
@@ -2,12 +2,18 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
+use std::thread;
+use std::time::Duration;
+use http_hello::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
     for stream in listener.incoming() {
-        let stream = stream.unwrap();
-        handle_connection(stream);
+        let stream = stream.unwrap();        
+        pool.execute(|| {
+            handle_connection(stream);
+        })        
     }
 }
 
@@ -16,13 +22,17 @@ fn handle_connection(mut stream: TcpStream) {
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
     
     let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
         ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
     } else { 
         ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
     }; 
-    
+
     let mut file = File::open(filename).unwrap();
     let mut contents = String::new();
     file.read_to_string(&mut contents).unwrap();
